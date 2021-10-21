40906c0952f4f98423e49c5c8ac52e2afffd7848
José Pedro Fernandes <josefernandesaiao@gmail.com>
2021-10-21 13:23:49 +0100

    update

---
+++ GetStarted/app/src/main/java/cmu/getstarted/MainActivity.kt
@@ -0,0 +1,11 @@
+package cmu.getstarted
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
