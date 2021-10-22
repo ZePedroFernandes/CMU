aeb85d910ecb061cfa042445db1e297c24d4dc75
José Pedro Fernandes <josefernandesaiao@gmail.com>
2021-10-22 15:47:24 +0100

    update

--- Ficha1/Pool/app/src/main/java/pt/ipp/estg/poolApp/MainActivity.kt
+++ Ficha1/Pool/app/src/main/java/pt/ipp/estg/poolApp/MainActivity.kt
@@ -2,10 +2,27 @@ package pt.ipp.estg.poolApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val chart = findViewById<LineChart>(R.id.chart)
+        var entries = ArrayList<Entry>()
+        for(i in 0..10){
+            entries.add(Entry(i.toFloat(),Math.random().toFloat()))
+        }
+
+        val dataSet = LineDataSet(entries,"label")
+        val lineData = LineData(dataSet)
+
+        chart.data = lineData
+        chart.invalidate()
     }
 }
