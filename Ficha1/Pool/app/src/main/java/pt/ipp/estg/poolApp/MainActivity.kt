a81b54d7b340994678ca22c60523a59ef601cf60
José Pedro Fernandes <josefernandesaiao@gmail.com>
2021-10-22 16:03:56 +0100

    ex2 feito

--- Ficha1/Pool/app/src/main/java/pt/ipp/estg/poolApp/MainActivity.kt
+++ Ficha1/Pool/app/src/main/java/pt/ipp/estg/poolApp/MainActivity.kt
@@ -2,6 +2,7 @@ package pt.ipp.estg.poolApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
@@ -9,20 +10,30 @@ import com.github.mikephil.charting.data.LineDataSet
 
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+    fun createNewChart() {
         val chart = findViewById<LineChart>(R.id.chart)
         var entries = ArrayList<Entry>()
-        for(i in 0..10){
-            entries.add(Entry(i.toFloat(),Math.random().toFloat()))
+        for (i in 0..10) {
+            entries.add(Entry(i.toFloat(), Math.random().toFloat()))
         }
 
-        val dataSet = LineDataSet(entries,"label")
+        val dataSet = LineDataSet(entries, "label")
         val lineData = LineData(dataSet)
 
         chart.data = lineData
         chart.invalidate()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        createNewChart()
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            createNewChart()
+        }
     }
 }
