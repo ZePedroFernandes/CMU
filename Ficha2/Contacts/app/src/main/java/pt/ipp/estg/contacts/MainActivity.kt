1698ea8e8688bf3ac52f5dd151f528256fd916bb
José Pedro Fernandes <josefernandesaiao@gmail.com>
2021-10-28 18:05:19 +0100

    uo

---
+++ Ficha2/Contacts/app/src/main/java/pt/ipp/estg/contacts/MainActivity.kt
@@ -0,0 +1,11 @@
+package pt.ipp.estg.contacts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
