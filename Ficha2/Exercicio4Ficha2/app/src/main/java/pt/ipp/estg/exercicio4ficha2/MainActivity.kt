5500f110eeb7c858b29bcef59980f688ea8f5f07
José Pedro Fernandes <josefernandesaiao@gmail.com>
2021-10-29 15:00:27 +0100

    up

---
+++ Ficha2/Exercicio4Ficha2/app/src/main/java/pt/ipp/estg/exercicio4ficha2/MainActivity.kt
@@ -0,0 +1,11 @@
+package pt.ipp.estg.exercicio4ficha2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
