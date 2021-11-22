72b80fbafb78074ac4b6e63e9efa70f0e31159a5
José Pedro Fernandes <josefernandesaiao@gmail.com>
2021-11-22 21:48:28 +0000

    a

---
+++ Ficha7/GasStationPrices/app/src/main/java/com/example/cmu_room/database/Converters.kt
@@ -0,0 +1,21 @@
+package com.example.cmu_room.database
+
+import androidx.room.TypeConverter
+import java.sql.Timestamp
+import java.time.LocalDateTime
+
+class Converters {
+
+    @TypeConverter
+    fun dateToTimestamp(date: LocalDateTime?): Long? {
+        return Timestamp.valueOf(date.toString()).time
+    }
+
+    @TypeConverter
+    fun fromTimestamp(value: Long?): LocalDateTime? {
+        value?.let {
+            return LocalDateTime.parse(Timestamp(value).toString())
+        }
+        return null
+    }
+}
---
+++ Ficha7/GasStationPrices/app/src/main/java/com/example/cmu_room/models/Fuel.kt
@@ -0,0 +1,24 @@
+package com.example.cmu_room.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+import java.time.LocalDateTime
+
+@Entity
+data class Fuel(
+    val fuelName: String,
+    val price: Double,
+    val date: LocalDateTime,
+    val gasStationId: Int,
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0,
+) : Serializable {
+
+    fun copy(
+        gasStationId: Int = this.gasStationId,
+        fuelName: String = this.fuelName,
+        price: Double = this.price
+    ) = Fuel(fuelName, price, this.date, this.gasStationId, this.id)
+
+}
